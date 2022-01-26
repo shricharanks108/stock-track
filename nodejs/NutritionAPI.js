d0e88cacfa6f5c71c7a80908652d7402d01e90fe
Fadi Farag <fadi50045000@gmail.com>
2022-01-26 00:20:44 -0500

    Added More Accessible Nutrition Functions

--- nodejs/NutritionAPI.js
+++ nodejs/NutritionAPI.js
@@ -12,29 +12,63 @@ function requestPromise(url){
 
 var NutritionAPI = {
     baseURL: "https://api.nal.usda.gov/fdc/v1",
-    getFood: async (id, wantsFullRes=false) => {
-        var res = await requestPromise(`${NutritionAPI.baseURL}/food/${id}?api_key=${FDC_API_KEY}`).catch(() => undefined);
-        return (wantsFullRes)? res:res.body;
+    getFood: async (fdcID) => {
+        var res = await requestPromise(`${NutritionAPI.baseURL}/food/${fdcID}?api_key=${FDC_API_KEY}`).catch(() => undefined);
+        if(res.body.length === 0) return null;
+        return JSON.parse(res.body);
     },
-    getFoods: async (IDs, wantsFullRes=false) => {
-        //TODO: check that IDs is nothing but digits
-        //TODO: NOT WORKING
-        fdcIDsURI = ""
-        for(var i = 0; i < IDs.length; i++){
-            if(i > 0) fdcIDsURI += "&";
-            fdcIDsURI += `fdcIds=${IDs[i]}`;
-        }
-        var res = await requestPromise(`${NutritionAPI.baseURL}/foods?${fdcIDsURI}?api_key=${FDC_API_KEY}`).catch(() => undefined);
-        return (wantsFullRes)? res:res.body;
+    getFoods: async (IDs) => {
+        //TODO: check that IDs is nothing but digits array
+        var res = await requestPromise(`${NutritionAPI.baseURL}/foods?fdcIds=${IDs.join(",")}&api_key=${FDC_API_KEY}`).catch(() => undefined);
+        if(res.body.length === 0) return null;
+        return JSON.parse(res.body);
     },
-    listFood: async (wantsFullRes=false) => {
+    listFood: async () => {
         var res = await requestPromise(`${NutritionAPI.baseURL}/foods/list?api_key=${FDC_API_KEY}`).catch(() => undefined);
-        return (wantsFullRes)? res:res.body;
+        if(res.body.length === 0) return null;
+        return JSON.parse(res.body);
     },
-    searchFood: async (food, wantsFullRes=false) => {
-        var res = await requestPromise(`${NutritionAPI.baseURL}/foods/search?api_key=${FDC_API_KEY}&query=${encodeURIComponent(food)}`).catch(() => undefined);
-        return (wantsFullRes)? res:res.body;
+    searchFood: async (food) => {
+        var res = await requestPromise(`${NutritionAPI.baseURL}/foods/search?api_key=${FDC_API_KEY}
+                                                                            &query=${encodeURIComponent(food)}
+                                                                            &dataType=Branded`)
+        .catch(() => undefined);
+        if(res.body.length === 0) return null;
+        return JSON.parse(res.body);
     },
+    isBranded: (foodData) => foodData && foodData.dataType === "Branded",
+    getServingSize: async (fdcID) => {
+        var foodData = await NutritionAPI.getFood(fdcID);
+        if(!NutritionAPI.isBranded(foodData)) return null;
+        return foodData.servingSize.toString() + foodData.servingSizeUnit;
+    },
+    getAllNutrients: async (fdcID) => {
+        var foodData = await NutritionAPI.getFood(fdcID);
+        // Food must be "Branded" in order to get nutrients
+        if(!NutritionAPI.isBranded(foodData)) return null;
+        return foodData.labelNutrients;
+    },
+    getNutrient: async (fdcID, nutrient) => {
+        if(typeof nutrient !== "string") return null;
+
+        var allNutrients = await NutritionAPI.getAllNutrients(fdcID);
+        if(allNutrients === null) return null;
+        return allNutrients[nutrient].value;
+    },
+    // manually get each of the label nutrients in case Frontend needs it to be stupid simple
+    getTotalFat: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "fat"),
+    getSaturatedFat: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "saturatedFat"),
+    getTransFat: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "transFat"),
+    getCholesterol: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "cholesterol"),
+    getSodium: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "sodium"),
+    getCarbohydrates: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "carbohydrates"),
+    getFiber: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "fiber"),
+    getSugars: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "sugars"),
+    getProtein: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "protein"),
+    getCalcium: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "calcium"),
+    getIron: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "iron"),
+    getPostassium: async (fdcID) => await NutritionAPI.getNutrient(fdcID, "postassium"),
+    getCalories: async(fdcID) => await NutritionAPI.getNutrient(fdcID, "calories"),
 }
 
 module.exports = NutritionAPI
