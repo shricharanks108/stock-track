5a3d095de31921064e3bfbea1e72f706ef36f7dd
VincentFox22 <95255013+VincentFox22@users.noreply.github.com>
2022-01-29 15:14:24 -0500

    Create Login Page

--- reactjs/src/pages/LoginPage/LoginPage.js
+++ reactjs/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,54 @@
 import './LoginPage.css';
+<<<<<<< Updated upstream
 import { Header } from '../../components/Header/Header';
 
 
 function LoginPage() {
   return (
     <div className="background">
+=======
+import profile from "./Images/LoginIcon.png";
+import email from "./Images/EmailIcon.png";
+import pass from "./Images/LockIcon.png";
+
+function LoginPage() {
+  return (
+    <div className="main">
+     <div className="sub-main">
+       <div>
+         <div className="imgs">
+           <div className="container-image">
+             <img src={profile} alt="profile" className="profile"/>
+
+           </div>
+
+
+         </div>
+         <div>
+           <h1>Login Page</h1>
+           <div>
+             <img src={email} alt="email" className="email"/>
+             <input type="text" placeholder="user name" className="name"/>
+           </div>
+           <div className="second-input">
+             <img src={pass} alt="pass" className="email"/>
+             <input type="password" placeholder="user name" className="name"/>
+           </div>
+          <div className="login-button">
+          <button>Login</button>
+          </div>
+           
+            <p className="link">
+              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
+            </p>
+           
+ 
+         </div>
+       </div>
+       
+
+     </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
