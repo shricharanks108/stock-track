2d5fd52c99d43a22c584eb5632b154abddb85f43
Shricharan KS <shricharanks108@gmail.com>
2022-04-18 01:18:04 -0400

    OrderFulfillment.js Merge Progress

--- reactjs/src/pages/OrderFulfillment/OrderFulfillment.js
+++ reactjs/src/pages/OrderFulfillment/OrderFulfillment.js
@@ -4,7 +4,48 @@ import './OrderFulfillment.css';
 
 function OrderFulfillment() {
 
+  let orders_to_fulfill = [
+    {
+      order_id: 1,
+      order_date: "2019-01-01",
+      order_status: "Pending",
+      order_total: "100.00",
+      order_items: [
+        {
+          product_id: 1,
+          product_name: "Test Product 1",
+          product_quantity: 1,
+          product_price: "10.00",
+        },
+        {
+          product_id: 2,
+          product_name: "Test Product 2",
+          product_quantity: 2,
+          product_price: "20.00",
+        }
+      ]
+    },
+    {
+      order_id: 2,
+      order_date: "2019-01-01",
+      order_status: "Pending",
+      order_total: "100.00",
+      order_items: [
+        {
+          product_id: 1,
+          product_name: "Test Product 1",
+          product_quantity: 1,
+          product_price: "10.00",
+        },
+      ]
+    },
+  ];
+
   let order_ids = [1, 2];
+
+  for (let i = 0; i < orders_to_fulfill.length; i++) {
+    order_ids.push(orders_to_fulfill[i]["order_id"]);
+  }
 
   return (
     <div>
@@ -17,7 +58,7 @@ function OrderFulfillment() {
         <Button>Pull Order for Fulfillment</Button>
         <div className="order-fulfillment-list-container">
           <div className="order-fulfillment-list-header">
-
+            <small>{order_ids.length}</small>
           </div>
         </div>
         <Button>Completed Order</Button>
@@ -25,6 +66,5 @@ function OrderFulfillment() {
     </div>
   );
 }
-
 
 export default OrderFulfillment;
