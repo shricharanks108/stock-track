b11105f26798e515e5e3295428265800c4379479
Shricharan KS <shricharanks108@gmail.com>
2022-04-26 22:06:21 -0400

    Add Routes for InventoryRoutes.js

--- nodejs/Routes/InventoryRoutes.js
+++ nodejs/Routes/InventoryRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 var bodyParser = require('body-parser');
 var session = require('express-session');
+const Inventory = require('.././DatabaseFunctions/Inventory');
 var connection = require(".././DatabaseFunctions/Database");
 
 async function setupConnection(){
@@ -26,12 +27,31 @@ router.use((req, res, next) => {
     next()
 });
 
-router.get('/products', async (req, res) => {
-
+router.get('/productIDsBySubcategory', async (req, res) => {
+  let result = await Inventory.getProductIDsBySubcategory(connection, req.body.pantryID, req.body.subcategory);
+  res.status(200).send({ "productIDs": result });
 });
   
-router.post('/products', async (req, res) => {
-
+router.get('/productIDsByMajorCategory', async (req, res) => {
+  let result = await Inventory.getProductIDsByMajorCategory(connection, req.body.pantryID, req.body.majorCategory);
+  res.status(200).send({ "productIDs": result });
 });
+
+router.get('/productsBySubcategory', async (req, res) => {
+  let result = await Inventory.getProductsBySubcategory(connection, req.body.pantryID, req.body.subcategory);
+  res.status(200).send({ "products": result });
+});
+
+router.get('/productsByMajorCategory', async (req, res) => {
+  let result = await Inventory.getProductsByMajorCategory(connection, req.body.pantryID, req.body.majorCategory);
+  res.status(200).send({ "products": result });
+});
+
+router.get('/pantryIDsFromProductID', async (req, res) => {
+  let result = await Inventory.getPantryIDsFromProductID(connection, req.body.productID);
+  res.status(200).send({ "pantryIDs": result });
+});
+
+
 
 module.exports = router;
