29403e5aa24a2c40f68963d73d4041f9b3d05a26
Shricharan KS <shricharanks108@gmail.com>
2022-04-26 20:32:51 -0400

    Finish Inventory.js Functions

--- nodejs/DatabaseFunctions/Inventory.js
+++ nodejs/DatabaseFunctions/Inventory.js
@@ -1,13 +1,5 @@
 class Inventory {
 
-    static async addProduct(connection, product, pantryId, productID) {
-
-    }
-
-    static async deleteProduct(connection, id, pantryId, productID) {
-
-    }
-
     static async getFoodPantryInventory(connection, id, pantryId) {
         if (typeof pantryId !== "number") return;
 
@@ -18,37 +10,85 @@ class Inventory {
         }
         return null;
     }
+    
+    static async addProduct(connection, product, pantryId, productID) {
 
-    static async getProductsByMajorCategory(connection, pantryId, category) {
+    }
 
+    static async deleteProduct(connection, id, pantryId, productID) {
+
+    }
+
+    static async getProductIDsBySubcategory(connection, pantryId, subcategory) {
+        if (typeof majorCategory !== "string" || typeof pantryId !== "number") return;
+
+        const [results, fields] = await connection.execute('SELECT * FROM products WHERE Subcategory = ?;', [subcategory]);
+
+        var productIDs = [];
+
+        if (results.length > 0) {
+            for (const result of results) {
+                productIDs.push(result['ProductID']);
+            }
+            return productIDs;
+        }
+        return null;
+    }
+
+    static async getProductIDsByMajorCategory(connection, pantryId, majorCategory) {
+        if (typeof majorCategory !== "string" || typeof pantryId !== "number") return;
+
+        const [results, fields] = await connection.execute('SELECT * FROM products WHERE PantryID = ? AND MajorCategory = ?;', [majorCategory]);
+
+        var productIDs = [];
+
+        if (results.length > 0) {
+            for (const result of results) {
+                productIDs.push(result['ProductID']);
+            }
+            return productIDs;
+        }
+        return null;
     }
 
     static async getProductsBySubcategory(connection, pantryId, subcategory) {
+        if (typeof majorCategory !== "string" || typeof pantryId !== "number") return;
 
-    }
-
-    static async getProductByID(connection, pantryId, productID) {
-        if (typeof pantryId !== "number") return;
-
-        const [results, fields] = await connection.execute('SELECT * FROM products WHERE food_pantry_ID = ? AND id = ?;', [pantryId, productID]);
+        const [results, fields] = await connection.execute('SELECT * FROM products WHERE PantryID = ? AND Subcategory = ?;', [subcategory]);
 
         if (results.length > 0) {
-            return results[0];
+            return results;
         }
         return null;
     }
 
-    static async getProductIDByName(connection, name, pantryId, productName) {
-        if (typeof pantryId !== "number") return;
+    static async getProductsByMajorCategory(connection, pantryId, majorCategory) {
+        if (typeof majorCategory !== "string" || typeof pantryId !== "number") return;
 
-        const [results, fields] = await connection.execute('SELECT * FROM products WHERE food_pantry_ID = ? AND name = ?;', [pantryId, name]);
+        const [results, fields] = await connection.execute('SELECT * FROM products WHERE PantryID = ? AND MajorCategory = ?;', [pantryId, majorCategory]);
 
         if (results.length > 0) {
-            return results[0];
+            return results;
         }
         return null;
     }
 
+    static async getPantryIDsFromProductID(connection, productID, majorCategory) {
+        if (typeof majorCategory !== "string" || typeof pantryId !== "number") return;
+
+        const [results, fields] = await connection.execute('SELECT * FROM products WHERE ProductID = ? AND MajorCategory = ?;', [productID, majorCategory]);
+
+        var pantryIDs = [];
+
+        if (results.length > 0) {
+            for (const result of results) {
+                pantryIDs.push(result['ProductID']);
+            }
+            return pantryIDs;
+        }
+        return null;
+    }
+    
 }
 
 module.exports = Inventory;
