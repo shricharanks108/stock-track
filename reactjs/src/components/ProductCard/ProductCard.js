4e46ee1c1a034fa15abb5bc265c8a0b76d827777
Shricharan KS <shricharanks108@gmail.com>
2022-05-18 13:44:49 -0400

    Finish up ProductListings.js

--- reactjs/src/components/ProductCard/ProductCard.js
+++ reactjs/src/components/ProductCard/ProductCard.js
@@ -12,14 +12,14 @@ function ProductCard(props) {
             <Card.Link>
                 <Card.Img variant="top" href={`/product/${id}`} src= {img}/>
                 <Card.Body>
-                <Card.Title style={{ textDecoration: 'none' }}className='product-card-title-text'>{product.name}</Card.Title>
+                <Card.Title style={{ textDecoration: 'none' }}className='product-card-title-text'>{product.ProductName}</Card.Title>
                 <Card.Text style={{ textDecoration: 'none' }} className='product-card-desc-text'>
-                    {product.description}
+                    {product.ProductDescription}
                 </Card.Text>
                 <AddToOrderButton productID={id} cartItems={cartItems} setCartItems={setCartItems}/>
                 <Breadcrumb>
-                    <Breadcrumb.Item href="#results-by-category">{product.category_1}</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#results-by-category">{product.category_2}</Breadcrumb.Item> 
+                    <Breadcrumb.Item href="#results-by-category">{product.MajorCategory}</Breadcrumb.Item>
+                    <Breadcrumb.Item href="#results-by-category">{product.Subcategory}</Breadcrumb.Item> 
                 </Breadcrumb>
                 </Card.Body>
             </Card.Link>
--- reactjs/src/pages/ProductListings/ProductListings.js
+++ reactjs/src/pages/ProductListings/ProductListings.js
@@ -1,44 +1,41 @@
 import './ProductListings.css';
 import { useState, useEffect } from 'react';
 import ProductCard from '../../components/ProductCard/ProductCard';
-import data from '../../data.js';
 import { Form, Button } from 'react-bootstrap';
 import Axios from 'axios';
 
 function ProductListings(props) {
 
-  var { products } = data;
-
-  // const [products, setProducts] = useState([]);
-  // const [isLoadingProducts, setLoadingProducts] = useState(true);
+  const [products, setProducts] = useState([]);
 
   const [majorCategory, setMajorCategory] = useState('Dairy');
   const [subcategory, setSubcategory] = useState();
   const [isLoadingSubcategories, setLoadingSubcategories] = useState(true);
   const [availableSubcategories, setAvailableSubcategories] = useState();
 
-  const filterProducts = () => {
-    Axios.get('https://stocktrack.shricharanks.com/inventory/productsByMajorCategory', {
-      headers: {
-        majorcategory: majorCategory,
-        subcategory: subcategory
-      }
-    }).then((res) => {
-      console.log(res.data.products);
-      products = res.data.products;
-    });
-  };
-
-  const getProducts = () => {
+  useEffect(() => {
     Axios.get('https://stocktrack.shricharanks.com/inventory/productsFromPantryID', {
-      headers: {
-        pantryid: 1
-      }
-    }).then((res) => {
-      if (res.data.subcategories.length > 0) {
-        products = res.data.products;
-      }
+        headers: {
+          pantryid: 1
+        }
+      }).then((res) => {
+        setProducts(res.data.products);
+        console.log(products);
+    }, () => {
+      console.log("error");
     });
+  }, []);
+
+  const filterProducts = () => {
+    // Axios.get('https://stocktrack.shricharanks.com/inventory/productsByMajorCategory', {
+    //   headers: {
+    //     majorcategory: majorCategory,
+    //     subcategory: subcategory
+    //   }
+    // }).then((res) => {
+    //   console.log(res.data);
+    //   products = res.data.products;
+    // });
   };
 
   useEffect(() => {
@@ -57,24 +54,6 @@ function ProductListings(props) {
     };
     availableOptions();
   }, [majorCategory]);
-
-  // useEffect(() => {
-  //   setLoadingProducts(true);
-  //   const availableProducts = async () => {
-  //     await Axios.get('https://stocktrack.shricharanks.com/inventory/productsFromPantryID', {
-  //       headers: {
-  //         pantryid: 1
-  //       }
-  //     }).then((res) => {
-  //       if (res.data.subcategories.length > 0) {
-  //         console.log(res.data.products);
-  //         setProducts(res.data.products);
-  //         setLoadingProducts(false);
-  //       }
-  //     });
-  //   };
-  //   availableProducts();
-  // }, [products]);
 
   return (
     <div>
@@ -110,15 +89,11 @@ function ProductListings(props) {
         </div>
       </div>
       <div className="products-row">
-        {/* {isLoadingProducts? <ProductCard className="" key={1} id={1} product={{id:1, name:"Loading", description:"Loading", category_1: "Loading", category_2: "Loading"}} cartItems={props.cartItems} setCartItems={props.setCartItems} ></ProductCard> :  products.map((product) => (
-          <ProductCard className="" key={product.id} id={product.id} product={product} cartItems={props.cartItems} setCartItems={props.setCartItems} ></ProductCard>
-        ))} */}
-        {console.log(products)}
         {products.map((product) => (
-          <ProductCard className="" key={product.id} id={product.id} product={product} cartItems={props.cartItems} setCartItems={props.setCartItems} ></ProductCard>
+          <ProductCard className="" key={product.ProductID} id={product.ProductID} product={product} cartItems={props.cartItems} setCartItems={props.setCartItems} ></ProductCard>
         ))}
       </div>
-      {/* <small>Showing all of {products.length} Product Results!</small> */}
+      <small>Showing all of {products.length} Product Results!</small>
     </div>
   );
 }
